bcb500f034e215842eb0bbacc8af1e9872efb658
Pedro Pablo Parapaino Garcia <pparapaino@gmail.com>
2023-06-12 19:19:52 -0400

    update bitacora by pedroçç

--- app/Http/Controllers/Admin/MarcaController.php
+++ app/Http/Controllers/Admin/MarcaController.php
@@ -12,7 +12,7 @@ use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Auth;
+
 use Carbon\Carbon;
 
 
